9e65f2e7a55a4570223a0df356dc1d0540683ddf
Blackwatch98 <danielduczyminski@gmail.com>
2025-12-31 00:22:15 +0100

    feat: Added sample project for trash operations + examples of using two operations

---
+++ samples/TrashOperations/Program.cs
@@ -0,0 +1,34 @@
+﻿using GoogleDriveApi_DotNet;
+
+// To see the difference use debugger
+
+using var cts = new CancellationTokenSource(TimeSpan.FromMinutes(5));
+
+using GoogleDriveApi gDriveApi = await GoogleDriveApi.CreateBuilder()
+    .SetCredentialsPath("credentials.json")
+    .SetTokenFolderPath("_metadata")
+    .SetApplicationName("QuickFilesLoad")
+    .BuildAsync(cancellationToken: cts.Token);
+
+string parentFolderId = "root";
+string sourceFolderName = "FileDownloader Test Folder";
+string? sourceFolderId = await gDriveApi.GetFolderIdByAsync(sourceFolderName, parentFolderId, cts.Token);
+if (sourceFolderId is null)
+{
+    Console.WriteLine($"Cannot find a folder with a name {sourceFolderName}.");
+    return;
+}
+
+string fileToDeleteName = "Fine";
+string? fileToDeleteId = await gDriveApi.GetFileIdByAsync(fileToDeleteName, sourceFolderId, cts.Token);
+if (fileToDeleteId is null)
+{
+    Console.WriteLine("Cannot find file.");
+    return;
+}
+
+// Move to trash bin
+await gDriveApi.MoveFileToTrashAsync(fileToDeleteId, cts.Token);
+
+// Restore from trash bin
+await gDriveApi.RestoreFileFromTrashAsync(fileToDeleteId, cts.Token);
