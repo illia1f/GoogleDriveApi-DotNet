2c4e2b68f543deb06ddc960fd9a84203ea901061
Blackwatch98 <danielduczyminski@gmail.com>
2026-01-02 15:28:23 +0100

    fix: Added missing InvalidFileTypeException constructor overload.

--- src/Exceptions/InvalidFileTypeException.cs
+++ src/Exceptions/InvalidFileTypeException.cs
@@ -5,5 +5,9 @@
         public InvalidFileTypeException() { }
         public InvalidFileTypeException(string message) : base(message) { }
         public InvalidFileTypeException(string message, Exception inner) : base(message, inner) { }
+        public InvalidFileTypeException(string fileId, string actualMimeType)
+        : base($"Invalid MIME type '{actualMimeType}' for item '{fileId}'. A file was expected.")
+        {
+        }
     }
 }
